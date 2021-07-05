25abb388b2fc21f0510ad313af39216d5dd651d1
Gaby1003 <garavito.tello.gabriella@gmail.com>
2021-07-04 22:14:02 -0500

    Post Service

---
+++ server.js
@@ -0,0 +1,13 @@
+const postService = require('./services/postService.js')
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.post('/addName', (req,res) =>  postService.addName(req, res))
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/public/index.html"));
+
+app.get('/addName', (req, res) => res.sendFile(__dirname + "/public/addName.html"));
